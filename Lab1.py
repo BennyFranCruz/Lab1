2ea8215f517a577224f3f45073592a858120349b
noahj9450hnson <LGJohnson11@gmail.com>
2023-01-26 13:24:39 -0800

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -6,8 +6,15 @@ def INIT():
     pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN_PP)
     tim4 = pyb.Timer(4, prescaler=0, period=0xFFFF)
     
-    ch1 = tim4.channel (1, pyb.Timer.PWM, pin=pinB6)
-    ch2 = tim4.channel (2, pyb.Timer.PWM, pin=pinB7)
+    ch1 = tim4.channel (1, pyb.Timer.ENC_AB, pin=pinB6)
+    ch2 = tim4.channel (2, pyb.Timer.ENC_AB, pin=pinB7)
     
-    
+def main():
+    while(True):
+        print(tim4.counter())
+        utime.sleep (.05)
+
+if __name__ == "__main__":
+    main()
+        
     
