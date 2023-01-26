6cb161577ba4ae8a48e9fea65475d40f89677b15
noahj9450hnson <LGJohnson11@gmail.com>
2023-01-26 14:03:36 -0800

    Update Lab1.py

--- Lab1.py
+++ Lab1.py
@@ -2,18 +2,33 @@ import pyb
 import utime
 
 def INIT():
-    pinB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN_PP)
-    pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN_PP)
+    pinB6 = pyb.Pin(pyb.Pin.board.PB6, pyb.Pin.IN)
+    pinB7 = pyb.Pin(pyb.Pin.board.PB7, pyb.Pin.IN)
     tim4 = pyb.Timer(4, prescaler=0, period=0xFFFF)
     
     ch1 = tim4.channel (1, pyb.Timer.ENC_AB, pin=pinB6)
     ch2 = tim4.channel (2, pyb.Timer.ENC_AB, pin=pinB7)
-    
-def main():
-    while(True):
-        print(tim4.counter())
-        utime.sleep (.05)
+    return tim4
 
+def main():
+    temp1 = 0
+    temp2 = 0
+    count = 0
+    delta = 0
+    period = 0xFFFF
+    
+    tim4 = INIT()
+    while(True):
+        temp2 = tim4.counter()
+        delta = temp2 - temp1
+        temp1 = temp2
+        if(delta > ((period + 1)/2)):
+            delta -= period + 1
+        elif(delta < (((-1 * period) + 1)/2)):
+            delta += period + 1 
+        count += delta
+        print(count)
+        utime.sleep (.5)
 if __name__ == "__main__":
     main()
         
